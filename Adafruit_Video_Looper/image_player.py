f187197ad2da889a093689bcd4ebe77e58df7666
Matt Adlard <adlard.matthew@gmail.com>
2024-08-24 05:30:34 +0100

    Update image_player.py
    Updates
    
    Explanation of the Changes:Image Caching and Eviction:An OrderedDict is used for the _image_cache, enabling LRU (Least Recently Used) caching._get_image_size estimates the memory usage of an image._evict_cache removes the oldest cached image when the cache exceeds the memory limit._add_to_cache adds new images to the cache while ensuring the cache does not exceed the size limit.Memory Management:The cache size is monitored using _current_cache_size, and eviction occurs if adding a new image would exceed the specified cache_size_limit.Performance Optimization:Frequently accessed images are prioritized in the cache by moving them to the end of the OrderedDict.This approach efficiently balances between memory usage and performance, ensuring that images are reused if possible, while also managing system memory consumption.

--- Adafruit_Video_Looper/image_player.py
+++ Adafruit_Video_Looper/image_player.py
@@ -1,39 +1,88 @@
-# Author: Tobias Perschon
+# Author: Tobias Perschon - Updated by Tavarc Design 
 # License: GNU GPLv2, see LICENSE.txt
-import os, pygame
+import import os
+import pygame
 from time import monotonic
+from collections import OrderedDict
 
 class ImagePlayer:
-
     def __init__(self, config, screen, bgimage):
-        """Create an instance of an image player uses pygame to display static images.
+        """
+        Initialize the ImagePlayer with configuration, screen, and background image.
         """
         self._load_config(config)
         self._screen = screen
-        self._loop = 0
-        self._startTime = 0
         self._bgimage = bgimage
-        self._isPaused = False
+        self._loop = 0
+        self._start_time = 0
+        self._image_cache = OrderedDict()  # Initialize the image cache with OrderedDict for LRU caching
+        self._cache_size_limit = config.getint('image_player', 'cache_size_limit') * 1024 * 1024  # Cache size limit in bytes
+        self._current_cache_size = 0
 
     def _load_config(self, config):
-        self._extensions = config.get('image_player', 'extensions') \
-                                 .translate(str.maketrans('', '', ' \t\r\n.')) \
-                                 .split(',')
+        """
+        Load configuration settings.
+        """
+        self._extensions = config.get('image_player', 'extensions').replace('.', '').split(',')
         self._duration = config.getint('image_player', 'duration')
         self._size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        self._bgcolor = list(map(int, config.get('video_looper', 'bgcolor')
-                                        .translate(str.maketrans('','', ','))
-                                        .split()))
+        self._bgcolor = [int(c) for c in config.get('video_looper', 'bgcolor').split(',')]
         self._scale = config.getboolean('image_player', 'scale') 
         self._center = config.getboolean('image_player', 'center') 
         self._wait_time = config.getint('video_looper', 'wait_time')
 
-    def supported_extensions(self):
-        """Return list of supported file extensions."""
-        return self._extensions
+    def _get_image_size(self, pyimage):
+        """
+        Estimate the memory size of the pygame
+        def _get_image_size(self, pyimage):
+        """
+        Estimate the memory size of a pygame surface (image) in bytes.
+        """
+        width, height = pyimage.get_size()
+        depth = pyimage.get_bytesize()  # Get bytes per pixel
+        return width * height * depth
+
+    def _evict_cache(self):
+        """
+        Evict the least recently used image from the cache to free up memory.
+        """
+        if self._image_cache:
+            _, evicted_image = self._image_cache.popitem(last=False)
+            self._current_cache_size -= self._get_image_size(evicted_image)
+
+    def _add_to_cache(self, image_path, pyimage):
+        """
+        Add an image to the cache. Evict old images if the cache size limit is exceeded.
+        """
+        image_size = self._get_image_size(pyimage)
+        
+        # Evict cache entries until there is enough space
+        while self._current_cache_size + image_size > self._cache_size_limit:
+            self._evict_cache()
+
+        # Add new image to cache
+        self._image_cache[image_path] = pyimage
+        self._current_cache_size += image_size
+
+    def _load_image(self, image_path):
+        """
+        Load an image from the cache or from disk if not already cached.
+        """
+        if image_path in self._image_cache:
+            # Move accessed image to the end of the cache (LRU)
+            pyimage = self._image_cache.pop(image_path)
+            self._image_cache[image_path] = pyimage
+        else:
+            # Load image from disk and add to cache
+            pyimage = pygame.image.load(image_path)
+            self._add_to_cache(image_path, pyimage)
+
+        return pyimage
 
     def play(self, image, loop=None, **kwargs):
-        """Display the provided image file."""
+        """
+        Display the provided image file.
+        """
         if loop is None:
             self._loop = image.repeats
         else:
@@ -41,11 +90,11 @@ class ImagePlayer:
         if self._loop == 0:
             self._loop = 1
         
-        imagepath = image.target
+        image_path = image.filename
 
-        if imagepath != "" and os.path.isfile(imagepath):
+        if image_path != "" and os.path.isfile(image_path):
             self._blank_screen(False)
-            pyimage = pygame.image.load(imagepath)
+            pyimage = self._load_image(image_path)  # Use cache or load image
             image_x = 0
             image_y = 0
             screen_w, screen_h = self._size
@@ -74,40 +123,38 @@ class ImagePlayer:
 
             self._screen.blit(pyimage, (image_x, image_y))
             pygame.display.flip()
-            #future todo: crossfade, ken burns possbile?
-            #future todo: maybe preload images and/or create pygame image dict
 
-        self._startTime = monotonic()
-
-    def pause(self):
-        self._isPaused = not self._isPaused
-    
-    def sendKey(self, key: str):
-        print("sendKey not available for image_player")
+        self._start_time = monotonic()
 
     def is_playing(self):
-        """Here we need to compare for how long the image was displayed"""
-        if self._loop <= -1 or self._isPaused: #loop one image = play forever
+        """
+        Check if the image is still being displayed based on the duration and loop count.
+        """
+        if self._loop <= -1:  # loop one image = play forever
             return True
         
-        playing = (monotonic() - self._startTime) < self._duration*self._loop
+        playing = (monotonic() - self._start_time) < self._duration * self._loop
         
-        if not playing and self._wait_time > 0: #only refresh background if we wait between images
+        if not playing and self._wait_time > 0:  # only refresh background if we wait between images
             self._blank_screen()
         
         return playing
 
     def stop(self, block_timeout_sec=0):
-        """Stop the image display."""
+        """
+        Stop the image display.
+        """
         self._blank_screen()
-        self._startTime = self._startTime-self._duration*self._loop
+        self._start_time = self._start_time - self._duration * self._loop
 
     def _blank_screen(self, flip=True):
-        """Render a blank screen filled with the background color and optional the background image."""
+        """
+        Render a blank screen filled with the background color and optionally the background image.
+        """
         self._screen.fill(self._bgcolor)
         if self._bgimage[0] is not None:
             self._screen.blit(self._bgimage[0], (self._bgimage[1], self._bgimage[2]))
-        if(flip):
+        if flip:
             pygame.display.flip()
 
     @staticmethod
@@ -116,5 +163,7 @@ class ImagePlayer:
 
 
 def create_player(config, **kwargs):
-    """Create new image player."""
+    """
+    Create new image player.
+    """
     return ImagePlayer(config, screen=kwargs['screen'], bgimage=kwargs['bgimage'])
